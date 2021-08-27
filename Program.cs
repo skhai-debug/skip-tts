4367653670ce51ddc4b719270579201e6d1c9b49
Skhai <sukokha@gmail.com>
2021-08-27 17:12:15 -0500

    git init

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Websocket.Client;
+
+namespace SkipTTS
+{
+    class Program
+    {
+        private static readonly ManualResetEvent ExitEvent = new ManualResetEvent(false);
+        private static string token;
+
+        static void Main(string[] args)
+        {
+            ReadToken();
+            var url = new Uri("wss://realtime.streamelements.com/socket.io/?cluster=main&EIO=3&transport=websocket");
+            using (var client = new WebsocketClient(url))
+            {
+                client.ReconnectTimeout = TimeSpan.FromSeconds(30);
+                client.ReconnectionHappened.Subscribe(info =>
+                    Console.WriteLine($"Reconnection happened, type: {info.Type}"));
+
+                client.MessageReceived.Subscribe(msg => {
+                    Console.WriteLine($"Message received: {msg}");
+                    string resp = Response(msg);
+                    if(resp != null)
+                    {
+                        Task.Run(() => {
+                            client.Send(resp);
+                            if (resp.Contains("skip"))
+                            {
+                                ExitEvent.Set();
+                            }
+                         });
+                    }
+                  });
+
+                client.Start().Wait();
+
+                
+                ExitEvent.WaitOne();
+            }
+        }
+
+        private static string Response(ResponseMessage message)
+        {
+            string msg = message.Text;
+            if (msg == "40")
+            {
+                return $"42[\"authenticate\",{{ \"method\":\"apikey\",\"token\":\"{token}\" }}]";
+            } else if (msg.Contains("authenticated"))
+            {
+                return "422[\"event:skip\",null]";
+            }
+            return null;
+        }
+
+        private static void ReadToken()
+        {
+            token = File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "token.txt"));
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,17 @@
+# StreamElements TTS Skipper
+A standalone program that skips StreamElements TTS donations. This program can be run in conjuction with something like [AutoHotKey](https://www.autohotkey.com/) to create a custom skip tts hotkey.
+
+## Setup
+Download the .exe from the releases page or build yourself.
+
+You may have to install [.NET 5 runtime](https://dotnet.microsoft.com/download/dotnet/5.0/runtime).
+
+Go to your streamelements [user page](https://streamelements.com/dashboard/account/channels) and get your overlay token.
+
+![Overlay Token](https://i.imgur.com/1CDI9Tf.png)
+
+Copy and paste your overlay token into a file named `token.txt` in the same folder as the program.
+
+![Token file](https://i.imgur.com/mUkQZfJ.png)
+
+Run the program and viola, annoying TTS skipped.
